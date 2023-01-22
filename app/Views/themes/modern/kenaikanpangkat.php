4a33ec929274339ee9f0164c34b6de2a24466be0
ariesrutung <ariesrutung@gmail.com>
2023-01-23 05:53:05 +0900

    filter angel

--- app/Views/themes/modern/kenaikanpangkat.php
+++ app/Views/themes/modern/kenaikanpangkat.php
@@ -25,6 +25,99 @@ helper('html'); ?>
               </div>
               <div class="col-sm-6">
                 <form method="get" action="" class="d-flex">
+                  <select id="struktural" onchange="" name="struktural" class="form-select struktural">
+                    <option value="">-- Pilih --</option>
+                    <option value="IV/a">V/a - IV/b</option>
+                    <option value="III/d">III/d ke bawah</option>
+                  </select>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="row">
+              <div class="col-sm-4 d-flex align-items-center">
+                <label for="struktural">Status Pengusulan</label>
+              </div>
+              <div class="col-sm-6">
+                <form method="get" action="" class="d-flex">
+                  <select name="struktural" class="form-select">
+                    <option value="">-- Pilih --</option>
+                    <option value="ACC">ACC</option>
+                    <option value="BTL">BTL</option>
+                    <option value="TMS">TMS</option>
+                    <option value="Proses Validasi">Proses Validasi</option>
+                  </select>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div class="card-body">
+            <?php
+            if (!empty($msg)) {
+              show_alert($msg);
+            }
+
+            $column = [
+              'ignore_search_urut' => 'No',
+              'nama' => 'Nama',
+              'nip' => 'NIP',
+              'pangkat' => 'Golongan/Ruang',
+              'jabatan' => 'Jabatan',
+              'jenis_jabatan' => 'Jenis Jabatan',
+              'prosedur' => 'Prosedur Kenaikan Pangkat',
+              'status' => 'Status',
+              'alasan' => 'Alasan',
+              // 'ignore_search_action' => 'Action'
+
+            ];
+
+            $settings['order'] = [2, 'asc'];
+            $index = 0;
+            $th = '';
+            foreach ($column as $key => $val) {
+              $th .= '<th>' . $val . '</th>';
+              if (strpos($key, 'ignore_search') !== false) {
+                $settings['columnDefs'][] = ["targets" => $index, "orderable" => false];
+              }
+              $index++;
+            }
+
+            ?>
+
+            <table id="tbl-struktural" class="table display table-striped table-bordered table-hover tbl-struktural" style="width:100%">
+              <thead>
+                <tr>
+                  <?= $th ?>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <?= $th ?>
+                </tr>
+              </tfoot>
+            </table>
+            <?php
+            foreach ($column as $key => $val) {
+              $column_dt[] = ['data' => $key];
+            }
+            ?>
+            <span id="dataTables-column" style="display:none"><?= json_encode($column_dt) ?></span>
+            <span id="dataTables-setting" style="display:none"><?= json_encode($settings) ?></span>
+            <span id="dataTables-url" style="display:none"><?= current_url() . '/getDataDT' ?></span>
+          </div>
+        </div>
+      </div>
+      <div class="tab-pane fade" id="tab-fungsionalumum" role="tabpanel" aria-labelledby="tab-fungsionalumum" tabindex="0">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="row">
+              <div class="col-sm-4 d-flex align-items-center">
+                <label for="struktural">Golongan/Ruang</label>
+              </div>
+              <div class="col-sm-6">
+                <form method="get" action="" class="d-flex">
                   <select name="struktural" class="form-select">
                     <option value="">-- Pilih --</option>
                     <option value="IV/a">V/a - IV/b</option>
@@ -86,7 +179,7 @@ helper('html'); ?>
 
             ?>
 
-            <table id="tbl-struktural-ivab" class="table display table-striped table-bordered table-hover" style="width:100%">
+            <table id="tbl-fungsionalumum" class="table display table-striped table-bordered table-hover" style="width:100%">
               <thead>
                 <tr>
                   <?= $th ?>
@@ -109,9 +202,100 @@ helper('html'); ?>
           </div>
         </div>
       </div>
-      <div class="tab-pane fade" id="tab-fungsionalumum" role="tabpanel" aria-labelledby="tab-fungsionalumum" tabindex="0">...</div>
 
-      <div class="tab-pane fade" id="tab-fungsionaltertentu" role="tabpanel" aria-labelledby="tab-fungsionaltertentu" tabindex="0">...</div>
+      <div class="tab-pane fade" id="tab-fungsionaltertentu" role="tabpanel" aria-labelledby="tab-fungsionaltertentu" tabindex="0">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="row">
+              <div class="col-sm-4 d-flex align-items-center">
+                <label for="struktural">Golongan/Ruang</label>
+              </div>
+              <div class="col-sm-6">
+                <form method="get" action="" class="d-flex">
+                  <select name="struktural" class="form-select">
+                    <option value="">-- Pilih --</option>
+                    <option value="IV/a">V/a - IV/b</option>
+                    <option value="III/d">III/d ke bawah</option>
+                  </select>
+                </form>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="row">
+              <div class="col-sm-4 d-flex align-items-center">
+                <label for="struktural">Status Pengusulan</label>
+              </div>
+              <div class="col-sm-6">
+                <form method="get" action="" class="d-flex">
+                  <select name="struktural" class="form-select">
+                    <option value="">-- Pilih --</option>
+                    <option value="ACC">ACC</option>
+                    <option value="BTL">BTL</option>
+                    <option value="TMS">TMS</option>
+                    <option value="Proses Validasi">Proses Validasi</option>
+                  </select>
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div class="card-body">
+            <?php
+            if (!empty($msg)) {
+              show_alert($msg);
+            }
+
+            $column = [
+              'ignore_search_urut' => 'No',
+              'nama' => 'Nama',
+              'nip' => 'NIP',
+              'pangkat' => 'Golongan/Ruang',
+              'jabatan' => 'Jabatan',
+              'jenis_jabatan' => 'Jenis Jabatan',
+              'prosedur' => 'Prosedur Kenaikan Pangkat',
+              'status' => 'Status',
+              'alasan' => 'Alasan',
+              // 'ignore_search_action' => 'Action'
+
+            ];
+
+            $settings['order'] = [2, 'asc'];
+            $index = 0;
+            $th = '';
+            foreach ($column as $key => $val) {
+              $th .= '<th>' . $val . '</th>';
+              if (strpos($key, 'ignore_search') !== false) {
+                $settings['columnDefs'][] = ["targets" => $index, "orderable" => false];
+              }
+              $index++;
+            }
+
+            ?>
+
+            <table id="tbl-fungsionaltertentu" class="table display table-striped table-bordered table-hover" style="width:100%">
+              <thead>
+                <tr>
+                  <?= $th ?>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <?= $th ?>
+                </tr>
+              </tfoot>
+            </table>
+            <?php
+            foreach ($column as $key => $val) {
+              $column_dt[] = ['data' => $key];
+            }
+            ?>
+            <span id="dataTables-column" style="display:none"><?= json_encode($column_dt) ?></span>
+            <span id="dataTables-setting" style="display:none"><?= json_encode($settings) ?></span>
+            <span id="dataTables-url" style="display:none"><?= current_url() . '/getDataDT' ?></span>
+          </div>
+        </div>
+      </div>
 
     </div>
   </div>
@@ -120,3 +304,20 @@ helper('html'); ?>
 
   </div>
 </div>
+
+
+<script>
+  $(document).ready(function() {
+    $('#table-result').DataTable();
+
+    function filterData() {
+      $('#table-result').DataTable().search(
+        $('.struktural').val()
+      ).draw();
+    }
+    $('.struktural').on('change', function() {
+      filterData();
+    });
+
+  });
+</script>
